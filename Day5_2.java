1bf13cc8b567d47358632fdde433824cfe7df309
JohanHero <31683169+JohanHero@users.noreply.github.com>
2018-08-18 14:31:19 +0200

    Add files via upload

---
+++ Day5_2.java
@@ -0,0 +1,60 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day5_2 {
+	
+//	private static int runner;
+//	private static int counter;
+//	private static int temp;
+	public static void main(String [] cmdLn) throws FileNotFoundException {
+		
+		
+		FileReader in = new FileReader("inputDay5.txt");
+		 
+		Scanner s = new Scanner(in);
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		
+			while ( s.hasNextLine()) {
+				
+				a.add(stringToInt(s.nextLine()));
+			
+			}
+			int temp = 0;
+			int prev = 0;
+			int counter = 0;
+			int index = 0;
+			
+			while (index < a.size()) {
+			  
+			temp = a.get(index);
+			
+			
+			if (temp >= 3) {
+				
+				prev = index;
+				index = index + temp;
+				a.set(prev,temp-1);
+				counter++;
+			}
+			else {
+			
+			prev = index;
+			index = index + temp;
+			a.set(prev,temp+1);
+			counter++;
+			}
+			
+			}
+			System.out.println(counter);
+	}
+			
+			
+	public static int stringToInt (String s ) {
+				int a = Integer.parseInt(s);
+				
+				return a;
+			}
+}
+	
